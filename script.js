8f31e3429322631dde9ed0144e4c1fc83f5b0a96
adarsh-j-1812 <155352001+adarsh-j-1812@users.noreply.github.com>
2025-02-06 23:40:14 +0530

    second commit

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
 let imag = document.querySelectorAll(".imag");
-let posi = [1, -310, 0, 310];
+let posi = [1, -310, 0, 310,512];
 Array.from(imag).forEach((e) => {
   let alts = e.getAttribute("id");
   let index = alts.split("-")[1];
-  e.style.left = (((window.innerWidth) / 2)-300) + posi[index] + "px";
+  e.style.left =
+    window.innerwidht / 2 -
+    e.getBoundingClientRect().width / 2 +
+    posi[index] +
+    "px";
 });
 
 function nextSlide() {
-    let firstelement = posi.shift();
-    posi.push(firstelement);
-    for (let i = 0; i < posi.length; i++) {
-      const e = imag[i];
-      e.style.zIndex = "1";
-      e.style.scale = "0.7";
-      e.style.left = (window.innerWidth - 300) / 2 + posi[i] + "px";
-      if (posi[i] === 0) {
-        e.style.zIndex = "9";
-        e.style.scale = "1.4";
-        // e.style.top = (((window.innerHeight-370)/2))+'px'
-      }
+  let firstelement = posi.shift();
+  posi.push(firstelement);
+  for (let i = 0; i < posi.length; i++) {
+    const e = imag[i];
+    e.style.zIndex = "1";
+    e.style.scale = "0.7";
+    e.style.left =
+      window.innerwidht / 2 -
+      e.getBoundingClientRect().width / 2 +
+      posi[i] +
+      "px";
+    if (posi[i] === 0) {
+      e.style.zIndex = "9";
+      e.style.scale = "1.4";
+      // e.style.top = (((window.innerHeight-370)/2))+'px'
     }
+  }
 }
-
-
 
 function prevSlide() {
-    let firstelement = posi.pop();
-    posi.unshift(firstelement);
-    for (let i = 0; i < posi.length; i++) {
-        const e = imag[i];
-        e.style.zIndex = "1";
-        e.style.scale = "0.7";
-        e.style.left = (window.innerWidth - 300) / 2 + posi[i] + "px";
-        if (posi[i] === 0) {
-          e.style.zIndex = "9";
-          e.style.scale = "1.4";
-          // e.style.top = (((window.innerHeight-370)/2))+'px'
-        }
-      }
+  let firstelement = posi.pop();
+  posi.unshift(firstelement);
+  for (let i = 0; i < posi.length; i++) {
+    const e = imag[i];
+    e.style.zIndex = "1";
+    e.style.scale = "0.7";
+    e.style.left =
+      window.innerwidht / 2 -
+      e.getBoundingClientRect().width / 2 +
+      posi[i] +
+      "px";
+    if (posi[i] === 0) {
+      e.style.zIndex = "9";
+      e.style.scale = "1.4";
+      // e.style.top = (((window.innerHeight-370)/2))+'px'
+    }
+  }
 }
-
-
-
